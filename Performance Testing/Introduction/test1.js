a7811f0a88f611f51809c423daf9ad760b4533fe
Patryk <ksiazekpatryk@gmail.com>
2026-07-14 16:57:00 +0200

    Fix URL in test1 function and add sleep between requests

--- Performance Testing/Introduction/test1.js
+++ Performance Testing/Introduction/test1.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import sleep from 'k6';
 
 export const options = {
     vus: 3,
@@ -6,5 +7,6 @@ export const options = {
     
 }
 export default function test1() {
-    http.get('https://quickpizzagrafana.com/');
+    http.get('https://quickpizza.grafana.com/');
+    sleep(1);
 }
