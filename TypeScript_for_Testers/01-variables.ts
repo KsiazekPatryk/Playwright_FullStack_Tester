bc95f92f6b090e45ffacf93910379245872f3632
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-07-21 10:03:17 +0200

    Add initial variables and logging to 01-variables.ts

---
+++ TypeScript_for_Testers/01-variables.ts
@@ -0,0 +1,16 @@
+const firstName = "Patrick";
+const age = 30;   
+const isAdult = true;
+const person = {
+    name: "Patrick Test",
+    age: 30,
+    isAdult: true
+}
+
+console.log(firstName);
+console.log(age);
+console.log(isAdult); 
+console.log(person);
+console.log(person.name);
+
+
