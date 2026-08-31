f75c6c023b2896f1b73c18ccf3da9720a2d3bee9
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-08-31 09:11:16 +0200

    Add string creation examples and concatenation demonstration in 04-strings.ts

--- TypeScript_for_Testers/04-strings.ts
+++ TypeScript_for_Testers/04-strings.ts
@@ -4,7 +4,21 @@ export {};
 
 const single = 'single quote';
 const double = 'double quote';
-
+const template = `template literal`;
+const multiLine = `This is
+a multi-line
+template literal`;
 
 console.log(single);
 console.log(double);
+console.log(template);
+console.log(multiLine);
+
+// String Concatenation and Template Literals
+const firstName = 'Patrick';
+const lastName =  'Testing';
+
+//Operator +
+console.log ('My name is' + firstName + ' ' + lastName);
+// Strings Templates
+console.log(`My name is ${firstName} ${lastName}`); 
