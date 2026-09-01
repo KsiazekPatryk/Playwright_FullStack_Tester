f0cfc8ec15debc3d432a7783e6ac94a1495b0a8b
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-09-01 14:56:43 +0200

    Add tests for login functionality with valid credentials in login.spec.ts; include visibility check for welcome message

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+//Opisujemy grupę testów z wykorzystaniem "describe"
+//Utworzenie hooka beforeEach, który będzie wykonywany przed każdym testem
 
 test.describe('Playwright page basic check', () => {
   test.beforeEach(async ({ page }) => {
@@ -7,15 +9,11 @@ test.describe('Playwright page basic check', () => {
 });
 
 test('has title', async ({ page }) => {
-  // await page.goto('https://playwright.dev/');
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  // await page.goto('https://playwright.dev/');
-
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
--- tests/the internet/login.spec.ts
+++ tests/the internet/login.spec.ts
@@ -18,4 +18,12 @@ test.describe('The Internet Home Page', () => {
         //Assert
         await expect(page.getByText(expectedMessage)).toBeVisible();
     });
+
+    test('login with valid credentials - record', async ({ page }) => {
+        await page.getByLabel('Username').fill('tomsmith');
+        await page.getByLabel('Password').fill('SuperSecretPassword!');
+        await page.getByRole('button', {name: 'Login'}).click();
+
+        await expect(page.getByRole('heading', {name: 'Welcome to the Secure Area.'})).toBeVisible();
+        });
 });
