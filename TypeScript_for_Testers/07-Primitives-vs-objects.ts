228ce31abe39a693003a37bbe154cf67c6492703
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-09-06 20:33:24 +0200

    Add examples for type casting with numbers and booleans in 07-Primitives-vs-objects.ts

--- TypeScript_for_Testers/07-Primitives-vs-objects.ts
+++ TypeScript_for_Testers/07-Primitives-vs-objects.ts
@@ -51,3 +51,30 @@ const undefinedValue: undefined = undefined;
 
 console.log(nullValue);
 console.log(undefinedValue);
+
+//rzutowanie typów - number
+
+console.log('rzutowanie typów - number');
+console.log(Number('42'));
+console.log(Number(''));
+console.log(Number('abc'));
+console.log(Number(true));
+console.log(Number(false));
+console.log(parseInt('42'));
+console.log(parseInt(''));
+console.log(parseInt('abc'));
+console.log(parseInt('null'));
+console.log(parseInt('undefined'));
+
+
+//rzutowanie typów - boolean
+
+console.log('rzutowanie typów - boolean');
+console.log(Boolean('true'));
+console.log(Boolean(''));
+console.log(Boolean('abc'));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+
