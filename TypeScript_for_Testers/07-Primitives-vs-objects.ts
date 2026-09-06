1d14c19cd4ac442be52abea748a620d745d66242
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-09-06 20:27:16 +0200

    Add examples for type casting and null/undefined handling in 07-Primitives-vs-objects.ts

--- TypeScript_for_Testers/07-Primitives-vs-objects.ts
+++ TypeScript_for_Testers/07-Primitives-vs-objects.ts
@@ -24,3 +24,30 @@ const newIsActive = new Boolean(false);
 console.log(newNumber);
 console.log(newName);
 console.log(newIsActive);
+
+if (newIsActive.valueOf()) {
+    console.log('Object wrapper this is active');
+}
+
+// rzutowanie typów - string  
+
+console.log('rzutowanie typów - string');
+const numberToString = 123;
+const booleanTruetoSting = true;
+const booleanFalseToString = false;
+
+console.log(String(numberToString));
+console.log(String(booleanTruetoSting));
+console.log(String(booleanFalseToString));
+
+console.log(numberToString.toString());
+console.log(booleanTruetoSting.toString());
+console.log(booleanFalseToString.toString());
+
+// null / undefined
+
+const nullValue: null = null;
+const undefinedValue: undefined = undefined;
+
+console.log(nullValue);
+console.log(undefinedValue);
