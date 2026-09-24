5a83930e56de33fde6156e20e08b1b7adbc8139d
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-09-24 14:42:20 +0200

    Update NaN demonstration in 09-NaN.ts and add new file 10-Null-Undefined.ts for null and undefined types

--- TypeScript_for_Testers/09-NaN.ts
+++ TypeScript_for_Testers/09-NaN.ts
@@ -4,6 +4,36 @@ export {};
 
 console.log (0/0);
 
-const five = '5';
+const five = 'n5';
 
 console.log(typeof five);
+
+console.log(typeof Number(five))
+console.log(typeof parseInt(five));
+
+
+console.log(Number(five));
+console.log(parseInt(five));
+
+
+//Ciekawostka !!
+ 
+console.log(String === String);
+console.log(Number === Number);
+console.log(Boolean === Boolean);
+
+//Porównanie NaN
+console.log(isNaN(NaN));
+console.log(isNaN(NaN));
+
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN('NaN'));
+console.log(Number.isNaN('abc'));
+console.log(Number.isNaN(123));
+
+
+//Sprawdzenie czy wartość jest liczbą!
+//Sprawdzenie typu nie wystarczy bo NaN jest typu 'number
+console.log(typeof five === 'number');
+
+
---
+++ TypeScript_for_Testers/10-Null-Undefined.ts
@@ -0,0 +1,18 @@
+export {};
+
+let session = null;
+let cache: string;
+
+console.log(`Cache: ${cache}`);
+//...//
+
+
+// Typ null i undefined
+session = null;
+console.log(`Sesja jest usunięta: ${session}`);
+console.log(`Cache: ${typeof cache}`);
+
+
+
+
+
