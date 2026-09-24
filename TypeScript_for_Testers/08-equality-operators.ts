4dae1f7cdc799dc30bcddef36c042b02d037b126
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-09-24 13:49:10 +0200

    Add equality operators example with null and undefined in 08-equality-operators.ts

---
+++ TypeScript_for_Testers/08-equality-operators.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+/*eslint-disable no-console */
+
+export {};
+
+const a = null;
+const b = undefined;
+
+console.log(typeof a);
+console.log(typeof b);
+
+console.log(a == b);
+console.log(a === b);
